b1277181d5658af75aff30d2f9799b86b9930923
Joe Booth <joseph.booth3@gmail.com>
2018-06-06 15:41:11 -0500

    adding function to allow manager to purchase more of a product and adding ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Bamazon
+
+Bamazon Customer allows a customer to peruse a list of products in the terminal and purchase the items.  Once they select the product and the quantity they want, then they receive the total cost of their purchase.
+
+Bamazon Manager allows a manager to update their inventory.  The manager can view current inventory, view products that are low in inventory, order more of an existing product or even add a new product to inventory.
--- bamazonManager.js
+++ bamazonManager.js
@@ -21,21 +21,27 @@ connection.connect(function (err) {
 
 function inventory() {
     inquirer.prompt([{
-        type: "list",
         name: "options",
+        type: "list",
         message: "What would you like to do?",
         choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "End Session"]
-    }]).then(function(ans) {
-        switch(ans.doTask) {
-            case "View Products for Sale": viewProducts();
-            break;
-            case "View Low Inventory": viewLowInventory();
-            break;
-            case "Add to Inventory": addInventory();
-            break;
-            case "Add New Product": addNewProduct();
-            break;
-            case "End Session": console.log("Ending Session");
+    }]).then(function (answer) {
+
+        switch (answer.options) {
+            case "View Products for Sale":
+                viewProducts();
+                break;
+            case "View Low Inventory":
+                viewLowInventory();
+                break;
+            case "Add to Inventory":
+                addInventory();
+                break;
+            case "Add New Product":
+                addNewProduct();
+                break;
+            case "End Session":
+                console.log("Ending Session");
         }
     })
 }
@@ -45,8 +51,7 @@ function viewProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         var productTable = [];
         for (var i = 0; i < res.length; i++) {
-            productTable[i] = 
-            {
+            productTable[i] = {
                 ID: res[i].item_id,
                 Product: res[i].product_name,
                 Department: res[i].department_name,
@@ -55,7 +60,7 @@ function viewProducts() {
             };
         }
         console.table('Product Inventory', productTable);
-           
+
     })
 }
 //function to display items with inventory count lower than 5
@@ -64,8 +69,7 @@ function viewLowInventory() {
         console.log("There are less than 5 in stock of the following products");
         var lowInventoryTable = [];
         for (var i = 0; i < res.length; i++) {
-            lowInventoryTable[i] = 
-            {
+            lowInventoryTable[i] = {
                 ID: res[i].item_id,
                 Product: res[i].product_name,
                 Department: res[i].department_name,
@@ -73,7 +77,88 @@ function viewLowInventory() {
                 Stock_Quantity: res[i].stock_quantity
             };
         }
+        console.table('Low Inventory', lowInventoryTable);
     })
 }
 
-viewLowInventory();
+function addInventory() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        inquirer.prompt([{
+                name: "product_name",
+                type: "list",
+                message: "Which product would you like to purchase?",
+                choices: function () {
+                    var choices = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choices.push(res[i].item_id + ": " + res[i].product_name)
+                    }
+                    return choices
+                }
+            },
+
+            {
+                name: "product_quantity",
+                type: "input",
+                message: "How many would you like to purchase?",
+                when: function (answers) {
+                    return answers.product_name
+                }
+            }
+        ]).then(function (answers) {
+
+            var splitSelectedItem = answers.product_name.split(":");
+            var selectedItem = splitSelectedItem[0];
+            var productQuantity = parseInt(answers.product_quantity);
+
+            connection.query("SELECT * FROM products WHERE ?", {
+                item_id: selectedItem
+            }, function (err, res) {
+                if (err) throw err;
+                itemQuantity = res[0].stock_quantity + productQuantity;
+                addedItem = res[0].product_name;
+                console.log("There are now " + itemQuantity + " " + addedItem + " in stock.");
+
+                connection.query("UPDATE products SET ? WHERE ?", [{
+                    stock_quantity: itemQuantity
+                }, {
+                    item_id: addedItem
+                }], function (err, res) {
+                    if (err) throw err;
+                    var table = cTable.getTable(
+                        {
+                            Product: addedItem,
+                            Stock_Quantity: itemQuantity
+                        }
+                    );
+                    console.log(table);
+                    additionalPurchase();
+                })
+            });
+
+            
+
+        });
+
+    });
+
+}
+
+function additionalPurchase() {
+    inquirer.prompt([{
+        name: "reply",
+        type: "confirm",
+        message: "Would you like to purchase another item?"
+    }]).then(function(ans) {
+        if(ans.reply) {
+            addInventory();
+        } else {
+            console.log("Inventory has been updated");
+        }
+    });
+}
+
+
+
+inventory();
